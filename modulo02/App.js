ada425599469e28721ef1f22b15a70cc69f3b701
Diego Alencar Jacober <57761933898>
2023-07-31 16:56:58 -0300

    iniciando desafio

--- modulo02/App.js
+++ modulo02/App.js
@@ -1,12 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+
+    const [name, setName] = useState()
+    const [idade, setIdade] = useState()
+    const [sexo, setSexo] = useState()
+    const [limit, setLimit] = useState(0)
+    const [student, setStudent] = useState(false)
+
     return (
-            <View style={styles.container}>
-                <StatusBar style="auto" />
-                <Text>App Vazio</Text>
-            </View>
+        <View style={styles.container}>
+            <StatusBar style="auto" />
+            <TextInput
+                style={styles.input}
+                onChangeText={setName}
+                value={name}
+                placeholder="Your name"
+                keyboardType="default"
+            />
+
+            <TextInput
+                style={styles.input}
+                onChangeText={setIdade}
+                value={idade}
+                placeholder="Your age"
+                keyboardType="default"
+            />
+        </View>
     );
 }
 
@@ -16,6 +38,13 @@ const styles = StyleSheet.create({
         backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
+    },
+
+    input: {
+        borderColor: 'darkblue',
+        borderBottomWidth: 1,
+        width: 250,
+        marginBottom: 20,
     },
 
 });
