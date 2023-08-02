aeeb8cb3aaad3217bd55f5e41f3752890696230d
Diego Alencar Jacober <57761933898>
2023-08-02 14:04:41 -0300

    Async storage

--- modulo02/App.js
+++ modulo02/App.js
@@ -1,49 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { useEffect, useState } from 'react';
-import { StyleSheet, Text, View, TextInput, TouchableOpacity, Keyboard } from 'react-native';
-import AsyncStorage from '@react-native-community/async-storage';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-
-    const [input, setInput] = useState("")
-    const [nome, setNome] = useState("")
-
-    useEffect(() => {
-        console.log('component did mount')
-    }, [])
-
-    function gravaNome() {
-        console.log('gravar', input)
-        setNome()
-        Keyboard.dismiss()
-    }
-
-    useEffect(() => {
-        console.log('mudou nome')
-    }, [nome])
-
-
     return (
-        <View style={styles.container}>
-            <StatusBar style="auto" />
-
-            <View style={styles.viewInput}>
-                <TextInput
-                    style={styles.input}
-                    value={input}
-                    onChange={(e) => console.log(e)}
-                    underlineColorAndroid="transparent"
-                />
-
-                <TouchableOpacity onPress={gravaNome}>
-                    <Text style={styles.botao}>+</Text>
-                </TouchableOpacity>
+            <View style={styles.container}>
+                <StatusBar style="auto" />
+                <Text>App Vazio</Text>
             </View>
-
-
-
-            <Text style={styles.nome}>{nome}</Text>
-        </View>
     );
 }
 
@@ -52,36 +15,7 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: '#fff',
         alignItems: 'center',
-        marginTop: 40,
-    },
-
-    viewInput: {
-        flexDirection: 'row',
-        alignItems: 'center',
-    },
-
-    input: {
-        width: 350,
-        height: 40,
-        borderColor: '#000',
-        borderWidth: 1,
-        padding: 10,
-    },
-
-    botao: {
-        backgroundColor: '#222',
-        color: '#fff',
-        height: 40,
-        alignItems: 'center',
         justifyContent: 'center',
-        padding: 10,
     },
 
-    nome: {
-        marginTop: 15,
-        fontSize: 30,
-        textAlign: 'center',
-        color: '#000',
-    },
 });
-
--- modulo02/asyncstorage.js
+++ modulo02/asyncstorage.js
@@ -1,12 +1,64 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, Keyboard } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
 
 export default function App() {
+
+    const [input, setInput] = useState()
+    const [nome, setNome] = useState()
+
+    useEffect(() => {
+        async function getData() {
+            await AsyncStorage.getItem('nome').then( (value) => {
+                setNome(value)
+            })
+        }
+
+        getData()
+    }, [])
+
+    function gravaNome() {
+        setNome(input)
+        Keyboard.dismiss()
+    }
+
+    useEffect( (_, prevState) => {
+
+        async function save() {
+            if(prevState !== nome) {
+                await AsyncStorage.setItem('nome', nome);
+            }
+        }
+
+        save()
+       
+    }, [nome])
+
+
     return (
-            <View style={styles.container}>
-                <StatusBar style="auto" />
-                <Text>App Vazio</Text>
+        <View style={styles.container}>
+            <StatusBar style="auto" />
+
+            <View style={styles.viewInput}>
+                <TextInput
+                    editable
+                    style={styles.input}
+                    onChangeText={e => setInput(e)}
+                    value={input}
+                    underlineColorAndroid="transparent"
+                />
+
+                <TouchableOpacity onPress={gravaNome}>
+                    <Text style={styles.botao}>+</Text>
+                
+                </TouchableOpacity>
             </View>
+
+
+
+            <Text style={styles.nome}>{nome}</Text>
+        </View>
     );
 }
 
@@ -15,8 +67,36 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: '#fff',
         alignItems: 'center',
-        justifyContent: 'center',
+        marginTop: 40,
     },
 
+    viewInput: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+
+    input: {
+        width: 350,
+        height: 40,
+        borderColor: '#000',
+        borderWidth: 1,
+        padding: 10,
+    },
+
+    botao: {
+        backgroundColor: '#222',
+        color: '#fff',
+        height: 40,
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 10,
+    },
+
+    nome: {
+        marginTop: 15,
+        fontSize: 30,
+        textAlign: 'center',
+        color: '#000',
+    },
 });
 
