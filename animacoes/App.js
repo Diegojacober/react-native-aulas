0f84321888f21b4a965f5a4c70c3c4b46aaeea82
Diego Alencar Jacober <57761933898>
2023-08-03 16:59:56 -0300

    Primeiros passos

--- animacoes/App.js
+++ animacoes/App.js
@@ -1,11 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
+import { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Animated } from 'react-native';
 
 export default function App() {
+
+  const [LarguraAnimada, setLarAnimada] = useState(new Animated.Value(150))
+  const [AlturaAnimada, setAltAnimada] = useState(new Animated.Value(50))
+
+  Animated.timing(
+    LarguraAnimada, {
+      toValue: 300,
+      duration: 2000, //milisegundos
+      useNativeDriver: false,
+    }
+  ).start()
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Animated.View style={{ width: LarguraAnimada, height: AlturaAnimada, backgroundColor: '#4169e1', justifyContent: 'center', }}>
+        <Text style={{ color: '#FFFF', fontSize: 25, textAlign: 'center' }}>Carregando...</Text>
+      </Animated.View>
     </View>
   );
 }
